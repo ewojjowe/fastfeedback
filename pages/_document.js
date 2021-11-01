ed3e4c280433cb33a9bdcbbc50cb75da6c4214ad
John Wilfred Montilde <wilfredjohnmontilde19@gmail.com>
2021-11-01 22:23:34 +0800

    random fixes

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
           <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
